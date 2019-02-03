b68c5125067896bd0c4a5beeccfd36232bd9b198
Bruce Olley <45301440+B-Olley@users.noreply.github.com>
2019-02-02 20:45:24 -0500

    A universal windows version of multiplication table
    This is a universal version of the multiplication table .
    I decided to add a date field. Just for a bit of fun.

---
+++ Universal Windows App/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace Stringpractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string repeat = "";
+        string label;
+        string dt1;
+        object datetime1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            label = "Multiplication Table";
+            
+            datetime1 = DateTime.Now.ToShortDateString();
+            dt1 = datetime1.ToString();
+            for (int i = 1; i != 11; i++)
+            {
+                for (int y = 1; y != 11; y++)
+                {
+                    int math1 = i * y;
+
+
+                    repeat += math1 + "\t";
+                }
+                repeat += "\n";
+            }
+
+
+
+
+
+
+            labelBox.Text = repeat;
+
+            textBox2.Text = label;
+           
+            date1.Text = dt1;
+
+
+        }
+
+
+        //public void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        //{
+           
+             
+        //}
+    }
+}
